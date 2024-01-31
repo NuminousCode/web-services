5a1dc22f5998dcdec7e5c2efe64a2c6765cc94ba
NuminousCode <sanchezwebdev@gmail.com>
2024-01-31 13:00:29 -0600

    Full desktop view 90%

--- components/Expertise.jsx
+++ components/Expertise.jsx
@@ -3,9 +3,9 @@ import styles from '../styles/Expertise.module.css'
 const Expertise = () => {
   return (
     <div className = {styles.containerExpertise}>
-      <h1 className = {styles.title}>Expertise</h1>
+      <h1 className = {styles.title}>Areas of Expertise</h1>
       <div className = {styles.content}>
-      MOBILE-FIRST DESIGN | HTML | CSS | JAVASCRIPT | REACT | NEXT.JS | NODE.JS | DEPLOYMENTS | DNS RECORDS | SQL | MONGO-DB | EXPRESS.JS | SEQUELIZE | HEADLESS CMS | SERVERLESS FUNCTIONS
+      MOBILE-FIRST DESIGN | HTML | CSS | JAVASCRIPT | JSON | REACT | NEXT.JS | NODE.JS | DEPLOYMENTS | DNS RECORDS | MYSQL | SQL SERVER | MONGO-DB | EXPRESS.JS | SEQUELIZE | HEADLESS CMS | SERVERLESS FUNCTIONS 
       </div>
     </div>
   )
--- components/Main.jsx
+++ components/Main.jsx
@@ -24,16 +24,17 @@ const Main = ({data, content}) => {
       <Hero data={data}/>
       <div className = {styles.cards}>
         <h1 className = {styles.rate}>Select Your Rate</h1>
+        <div className = {styles.cardsInner}>
         <h2 className = {styles.titleCard}>Hourly</h2>
         <Card data = {contentCard1} gradient = {gradient1}/>
         <h2 className = {styles.titleCard}>Daily</h2>
         <Card data = {contentCard2} gradient = {gradient2}/>
         <h2 className = {styles.titleCard}>Project</h2>
         <Card data = {contentCard3} gradient = {gradient1}/>
+        </div>
         <Expertise />
         <Button label={labelContact} className={styles.button}/>
       </div>
-      
     </div>
   )
 }
