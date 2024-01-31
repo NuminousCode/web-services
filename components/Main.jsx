5295e896299f96551aaad0879398a45b19d49794
NuminousCode <sanchezwebdev@gmail.com>
2024-01-31 16:28:41 -0600

    Added card titles to desktop view

--- components/Main.jsx
+++ components/Main.jsx
@@ -24,6 +24,11 @@ const Main = ({data, content}) => {
       <Hero data={data}/>
       <div className = {styles.cards}>
         <h1 className = {styles.rate}>Select Your Rate</h1>
+          <div className = {styles.desktopTitleCards}>
+            <h2 className = {styles.desktopTitleCard}>Hourly</h2>
+            <h2 className = {styles.desktopTitleCard}>Daily</h2>
+            <h2 className = {styles.desktopTitleCard}>Project</h2>
+          </div>
         <div className = {styles.cardsInner}>
         <h2 className = {styles.titleCard}>Hourly</h2>
         <Card data = {contentCard1} gradient = {gradient1}/>
