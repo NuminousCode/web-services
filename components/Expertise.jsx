401b72b12a922a3bc95dd6cdae6c352fe12d5954
NuminousCode <sanchezwebdev@gmail.com>
2024-02-04 17:26:21 -0600

    expertise componenet mods

--- components/Expertise.jsx
+++ components/Expertise.jsx
@@ -5,7 +5,7 @@ const Expertise = () => {
     <div className = {styles.containerExpertise}>
       <h1 className = {styles.title}>Areas of Expertise</h1>
       <div className = {styles.content}>
-      WEBSITE DESIGN | MOBILE-FIRST DESIGN | DATABASES | CONTENT MANAGEMENT | SEO | HTML | CSS | JAVASCRIPT | REACT | NEXT.JS | MYSQL | SQL SERVER | MONGO-DB | JSON | NODE.JS | DEPLOYMENTS | DNS RECORDS | EXPRESS.JS | SEQUELIZE | HEADLESS CMS | SERVERLESS FUNCTIONS 
+      WEBSITE DESIGN | MOBILE-FIRST DESIGN | DATABASES | HOSTING | CONTENT MANAGEMENT | SEO | HTML | CSS | JAVASCRIPT | REACT | NEXT.JS | MYSQL | SQL SERVER | MONGO-DB | JSON | NODE.JS | EXPRESS.JS | SEQUELIZE | CLOUD COMPUTING
       </div>
     </div>
   )
